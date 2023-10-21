0ad816a47393d3446e98496901ae1b669081051f
Aditya Mishra <am14156789@gmail.com>
2023-10-21 22:29:36 +0530

    Learned about void Pointer

--- Harry_C.c
+++ Harry_C.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> //Used for Dynamic Memory allocation
 #include <conio.h>
 
 
@@ -15,7 +15,7 @@ void maly(){
     static int c = 5;
     printf("Static variable c=%d\n",c);
     c++;
-    register ai =4578;
+    register int ai =4578;
     printf("Register variable ai=%d\n",ai);
 }
 
@@ -24,64 +24,78 @@ void maly(){
 
 int main()
 {
-    int *ptr;
-    int a ;
-    scanf("%d",&a);
-    ptr = (int *)malloc(a*sizeof(int));
-    for (int i = 0; i < a; i++)
-    {
-        scanf("%d",&ptr[i]);
-    }printf("\n");
-    for (int i = 0; i < a; i++)
-    {
-        printf("%d\n",ptr[i]);
-    }
+    // -------------------------------Dynamic Storage Class-----------------------?
+//     int *ptr;
+//     int a ;
+//     scanf("%d",&a);
+//     ptr = (int *)malloc(a*sizeof(int));
+//     for (int i = 0; i < a; i++)
+//     {
+//         scanf("%d",&ptr[i]);
+//     }printf("\n");
+//     for (int i = 0; i < a; i++)
+//     {
+//         printf("%d\n",ptr[i]);
+//     }
     
     
-    printf("\nMalloc passed\n");
+//     printf("\nMalloc passed\n");
 
 
-    int *ptr_c;
-    int b ;
-    scanf("%d",&b);
-    ptr_c = (int *)calloc(b,sizeof(int));
-    for (int i = 0; i < b; i++)
-    {
-        scanf("%d",&ptr_c[i]);
-    }printf("\n");
-    for (int i = 0; i < b; i++)
-    {
-        printf("%d\n",ptr_c[i]);
-    }
+//     int *ptr_c;
+//     int b ;
+//     scanf("%d",&b);
+//     ptr_c = (int *)calloc(b,sizeof(int));
+//     for (int i = 0; i < b; i++)
+//     {
+//         scanf("%d",&ptr_c[i]);
+//     }printf("\n");
+//     for (int i = 0; i < b; i++)
+//     {
+//         printf("%d\n",ptr_c[i]);
+//     }
     
     
-    printf("\nCalloc passed\n");
-//  -------------------------------Realloc_____________
-    printf("Enter new Size of Malloc Array: ");
-    int e;
-    scanf("%d",&e);
-    ptr=(int*)realloc(ptr,e*sizeof(int));
-    if(ptr==NULL)
-    {
-        printf("Memory not allocated \n");
-        }
-        else
-        {
-            for (int i = 0; i < e; i++)
-        {
-            scanf("%d",&ptr[i]);
-        }printf("\n");
-        for (int i = 0; i < e; i++)
-        {
-            printf("%d\n",ptr[i]);
-        }
-            printf("Memory successfully reallocated \n");
-            }
-            // ----------------------------Free ___________________
-            free(ptr);
-            printf("Memory freed %d & %d\n",&ptr,ptr);
+//     printf("\nCalloc passed\n");
+// //  -------------------------------Realloc_____________
+//     printf("Enter new Size of Malloc Array: ");
+//     int e;
+//     scanf("%d",&e);
+//     ptr=(int*)realloc(ptr,e*sizeof(int));
+//     if(ptr==NULL)
+//     {
+//         printf("Memory not allocated \n");
+//         }
+//         else
+//         {
+//             for (int i = 0; i < e; i++)
+//         {
+//             scanf("%d",&ptr[i]);
+//         }printf("\n");
+//         for (int i = 0; i < e; i++)
+//         {
+//             printf("%d\n",ptr[i]);
+//         }
+//             printf("Memory successfully reallocated \n");
+//             }
+//             // ----------------------------Free ___________________
+//             free(ptr);
+//             printf("Memory freed %d & %d\n",&ptr,ptr);
 
-// -------------------------------Storage Classes---------------------
-    maly();maly();maly();maly();
+// // -------------------------------Storage Classes---------------------
+//     maly();maly();maly();maly();
+
+
+    // ==========================   Void Pointer------------------------
+    int ptr_a= 345;
+    float b_pr = 4.65;
+    void *ptr_1;
+    ptr_1 = &ptr_a;
+    printf("Value of ptr_a is %d\n",*(int *)ptr_1);
+    ptr_1 = &b_pr;
+    printf("Value of b_pr is %f\n",*(float *)ptr_1);
+
+// Used in malloc() and calloc() at starting as {(int *) or (char *)}
+
     return 0;
 }
--- starting.cpp
+++ starting.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 using namespace std;
-int main(){
-int c;
-std::cin>>c;
-cout<<c<<"\n";
-for(int i= 0; i<=c; i++){
-    cout<<i<<endl;
+int s(int a, int k) {
+    int m = (a - 1) / k;
+    return m * (m + 1) / 2 * k;
 }
-return 0;
+int main(){
+    int co= s(1000000000,3)+s(1000000000,5)-s(1000000000,15);
+    cout<<co;
+    int c;
+    std::cin>>c;
+    cout<<c<<"\n";
+    for(int i= 0; i<=c; i++){
+        cout<<i<<endl;
+    }
+    return 0;
 }
