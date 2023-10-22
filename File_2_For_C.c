8c18a1fa271e5fc6e52fd87cf3e502c93c0889fb
Aditya Mishra <am14156789@gmail.com>
2023-10-22 14:24:03 +0530

    Added

--- File_2_For_C.c
+++ File_2_For_C.c
@@ -6,15 +6,20 @@
 int main(){
     main2();//#include file included Harry_C content
     int ro = 34;
-    printf("Area of radius %d via #defineD area is :%f \n",ro,Circle_Area(ro));
+    printf("Area of radius %d via #defined area is :%f \n",ro,Circle_Area(ro));
 #ifdef Debug 
     printf("debugging\n");
 #endif
 #undef Debug
-#ifdef Debug 
-    printf("Undefinedd debug\n");
+#ifndef Debug 
+    printf("Undefined debug\n");
 #else 
     pro("Define works perfectly\n");
+#endif
+
+// ++++++++++++++++ Used Predifned Macros------------------- 
+#if True == 1
+printf("The current file is %s running on date %s at time %s on line %f return %d and STDC as %d",__FILE__,__DATE__,__TIME__,__LINE__,__STDC_VERSION__,__STDC__);
 #endif
     return 0;
 }
