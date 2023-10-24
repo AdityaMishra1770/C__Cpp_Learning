75b3036e3a8ef614ea37dfb7a04c04d582957830
Aditya Mishra <am14156789@gmail.com>
2023-10-24 20:01:15 +0530

    Command Added

---
+++ cmd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(int argc, char const *argv[]){
+    printf("The value of argc is: %d\n",argc);
+    printf("Values Added are: \n");
+    for (int i = 0; i <= argc; i++)
+    {
+        printf("%s\n",argv[i]);
+    }
+    
+    return 0;
+}
