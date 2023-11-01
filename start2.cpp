0c403117fd35047e6e209be971bd8850f26e81a1
Aditya Mishra <am14156789@gmail.com>
2023-11-01 07:22:51 +0530

    Added 1 file

---
+++ start2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[]){
+    // Bitwise Operator :
+    ///Bitwise compares every single binary place according to operand and results acc. to truth table
+    cout<<(2&4)<<endl; 
+    cout<<(2&&4)<<endl; 
+    cout<<(2|4)<<endl; 
+    cout<<(2||4)<<endl; 
+    cout<<(~10)<<endl; 
+    return 0;
+}
